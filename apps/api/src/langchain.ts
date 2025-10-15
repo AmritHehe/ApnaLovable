a0f8162c4079011d79d818827e2f7e0acf311cc0
AmritHehe <hopesalive1506@gmail.com>
2025-10-15 21:15:12 +0530

    basic e2b and langchain (self learning)

--- apps/api/src/langchain.ts
+++ apps/api/src/langchain.ts
@@ -1,6 +1,12 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import { SystemMessage , HumanMessage } from "@langchain/core/messages";
 import { ChatPromptTemplate, PromptTemplate } from "@langchain/core/prompts";
+import{ tool } from "@langchain/core/tools"
+import {z} from "zod"
+import { sandbox } from "./e2b";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+//what tools should a llm need ? 
+//create file read file update file delete file 
 
 const model = new ChatGoogleGenerativeAI({ 
     model : "gemini-2.0-flash", 
@@ -11,25 +17,44 @@ const model = new ChatGoogleGenerativeAI({
 //     new SystemMessage("Translate the following from English to Hindi , respond in one word only "),
 //     new HumanMessage("hi!")
 // ]
-const systemTemplate = "Translate the following from English to {language}"
+const systemTemplate = "You are a helpful assistant that can execute next js landing page  code in a secured sanboxed environment  Only respond with the code to be executed and nothing else. just give raw code, dont give anything else except code ."
 const promtTemplate = ChatPromptTemplate.fromMessages([
     ["system" , systemTemplate],
     ["user" , "{text}"],
 ])
+const text = "create a good landing page"
+const outputParser = new StringOutputParser(); 
+const chain = promtTemplate.pipe(model).pipe(outputParser)
 
-const promtValue = await promtTemplate.invoke({ 
-    language : "hindi" , 
-    text : "hi"
-})
+const code  = await chain.invoke({text})
 
 // const response = await model.invoke(messages)
 // console.log("response : " + console.log(response.content))
 
-const res = promtValue.toChatMessages(); 
-console.log("to chat message" + JSON.stringify(res))
-const stream = await model.stream(promtValue) 
-const chunks  :any = []; 
-for await (const chunk of stream ){ 
-    chunks.push(chunk); 
-    console.log(`${chunk.content} | ` )
+// const res = promtValue.toChatMessages(); 
+
+// console.log("to chat message" + JSON.stringify(res))
+
+// const stream = await model.stream(code) 
+// const chunks  :any = []; 
+// for await (const chunk of stream ){ 
+//     chunks.push(chunk); 
+//     console.log(`${chunk.content} | ` )
+// }
+// Strip backticks if present
+function stripCodeFences(code: string) {
+  // Remove opening ``` and optional language
+  code = code.replace(/^```[a-z]*\s*/, '');
+  // Remove closing ```
+  code = code.replace(/```\s*$/, '');
+  return code.trim();
 }
+
+const cleanedCode = stripCodeFences(code)
+
+console.log('code :' + cleanedCode )
+const exec = await sandbox.runCode(cleanedCode)   ; 
+console.log("output : " , exec);
+
+
+// await sandbox.kill()
